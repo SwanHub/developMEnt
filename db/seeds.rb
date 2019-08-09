337b44dbb4219b5de3cf821ad0a039b402c7933c
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-08-09 14:57:05 -0600

    cody added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,6 +30,7 @@ new_job = Aspiration.create(user_id: cory[:id],
                   milestone_title_5: "Learn new language",
                   milestone_content_5: "Some good options: python, go, haskell")
 swol = Aspiration.create(user_id: blitzen[:id],
+                  aspiration: "Get into better shape"
                   title: "Get swol",
                   time_frame: "10 weeks",
                   kind: "Personal",
@@ -43,3 +44,66 @@ swol = Aspiration.create(user_id: blitzen[:id],
                   milestone_content_4: "Stilt running",
                   milestone_title_5: "Pecs",
                   milestone_content_5: "Gorilla pounding and terry crews moves.")
+
+eat_healthier = Aspiration.create(user_id: cory[:id],
+                  aspiration: "Eat healthier "
+                  title: "No more junk food",
+                  time_frame: "2 weeks",
+                  kind: "Personal",
+                  milestone_title_1: "Remove junk food",
+                  milestone_content_1: "Take out snacks in pantry",
+                  milestone_title_2: "Buy vegetables",
+                  milestone_content_2: "Got to farmers market and get some veggies",
+                  milestone_title_3: "Don't buy more junk food",
+                  milestone_content_3: "Don't even go down the candy aisle",
+    
+pro_skier = Aspiration.create(user_id: cory[:id],
+                  aspiration: "Become a professinal skier"
+                  title: "Practice skiing",
+                  time_frame: "16 weeks",
+                  kind: "Personal",
+                  milestone_title_1: "Turning",
+                  milestone_content_1: "Practice runs through slalom course ",
+                  milestone_title_2: "Speed",
+                  milestone_content_2: "Bomb down the hill and don't turn",
+                  milestone_title_3: "Get some air",
+                  milestone_content_3: "Go to terrain park and practice hitting jumps",
+                  milestone_title_4: "Get on chairlift",
+                  milestone_content_4: "Don't fall while trying to get onto chairlift",
+            
+project_management = Aspiration.create(user_id: mandy[:id],
+                  aspiration: "Improve project management",
+                  title: "Git organized",
+                  time_frame: "4 weeks",
+                  kind: "Professional",
+                  milestone_title_1: "Find course",
+                  milestone_content_1: "Go online and find a good pm course",
+                  milestone_title_2: "Take course",
+                  milestone_content_2: "Sign up and attend pm course",
+                  milestone_title_3: "Pass course",
+                  milestone_content_3: "Do not fail this course",
+                  milestone_title_4: "Implement learnings",
+                  milestone_content_4: "Use your new knowledge",
+                  milestone_title_5: "Share with others",
+                  milestone_content_5: "Enlighten your co-workers")
+
+travel_more = Aspiration.create(user_id: mandy[:id],
+                  aspiration: "Travel more",
+                  title: "Travel to new places",
+                  time_frame: "35 weeks",
+                  kind: "Personal",
+                  milestone_title_1: "Italy",
+                  milestone_content_1: "Travel to Italy",
+                  milestone_title_2: "France",
+                  milestone_content_2: "Travel to France",
+                  milestone_title_3: "Belgium",
+                  milestone_content_3: "Travel to Belgium",
+                  milestone_title_4: "Antartica",
+                  milestone_content_4: "Travel to Antartica",
+                  milestone_title_5: "Greenland",
+                  milestone_content_5: "Travel to Greenland")
+
+
+
+                  
+
